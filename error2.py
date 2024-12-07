4edac9f3e696c6849cb8430b700777d8fd324e7b
triveni <trivenibatthini@gmail.com>
2024-12-07 15:03:12 +0530

    first

---
+++ error2.py
@@ -0,0 +1,8 @@
+def Calculate_function(numbers):
+    total=sum(numbers)
+    average=total/  len(numbers)
+    return average
+
+number=[10 , 20 ,30]
+result=Calculate_function(number)
+print("Average",result)
